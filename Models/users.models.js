9d86f7b31f0a558fc2bd51cea6f4a6a36e47085b
Onuryilmaz24 <onurr.yilmaz24@gmail.com>
2024-11-27 22:14:33 +0000

    Task-16-selectuserbyId COmpleted

--- Models/users.models.js
+++ Models/users.models.js
@@ -7,3 +7,14 @@ exports.selectAllUsers = () => {
     return rows;
   });
 };
+
+exports.selectUserByUsername = (username) => {
+  const sqlText = `SELECT * FROM users WHERE username = $1`
+
+  const values = [username]
+
+
+  return db.query(sqlText,values).then(({rows})=>{
+    return rows[0]
+  })
+}
--- Routers/users.router.js
+++ Routers/users.router.js
@@ -1,8 +1,10 @@
-const { getAllUsers } = require("../Controller/users.controller");
+const { getAllUsers, getUserById } = require("../Controller/users.controller");
 
 
 const usersRouter = require("express").Router();
 
 usersRouter.get("/",getAllUsers)
+
+usersRouter.get("/:username",getUserById)
 
 module.exports = usersRouter
